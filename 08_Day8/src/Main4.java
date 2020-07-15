2d03bf6ca34f443516fe784a1356ba7da7cc2382
Song minseon <minseonsong4@gmail.com>
2020-07-15 17:13:04 +0900

    200715. Day8

---
+++ 08_Day8/src/Main4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
