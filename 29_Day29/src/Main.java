aa4863b230b8ac8676a7db1c7b215dff69d8c88b
Song minseon <minseonsong4@gmail.com>
2020-08-15 21:15:15 +0900

    200815.Day29

---
+++ 29_Day29/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int arr[] = new int[9];
+		
+		int max = 0;
+		int index = 0;
+		
+		for(int i=0; i<9; i++) {
+			arr[i]  = sc.nextInt();
+			if(arr[i]>max) {
+				max = arr[i];
+				index = i+1;
+			}
+		}
+		
+		System.out.println(max);
+		System.out.println(index);
+	}
+
+}
