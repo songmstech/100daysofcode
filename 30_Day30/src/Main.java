8e4a08e97beb73e2aeee1ee23d1d0ae704c9b77f
Song minseon <minseonsong4@gmail.com>
2020-08-17 21:10:51 +0900

    200817.Day30

---
+++ 30_Day30/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 int[] arr = new int[10];
+		 
+		 for(int i=0; i < 10; i++) {
+			 arr[i] = i + 1;
+			 System.out.println(arr[i]);
+		 }
+		 
+		 for(int i=9; i >= 0; i--) {
+			 arr[i] = i + 1 ;
+			 System.out.println(arr[i]);
+		 }
+	}
+
+}
