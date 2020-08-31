fce8b1a74e0cfa62539d09b24311a347994691a9
Song minseon <minseonsong4@gmail.com>
2020-08-31 22:37:18 +0900

    200831.Day39

---
+++ 39_Day39/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int index = 0;
+		int[] arr = new int[n];
+		while(st.hasMoreTokens()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
+		}
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[n - 1]);
+	}
+
+}
